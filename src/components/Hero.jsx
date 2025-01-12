fe4f8b17f59c59626e8a6fb063c5fdd11a6538b9
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-01-12 15:15:07 +0100

    hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,14 +2,22 @@ function Hero() {
     return (
         <div className="banner w-full flex flex-col justify-end items-center max-sm:h-[550px] max-sm:gap-2">
             <h2 className="text-white text-center text-6xl font-bold tracking-[1.86px] leading-[60px] max-sm:text-4xl max-[400px]:text-3xl">
-                Discover the Best <br />
-                Abaya
+                Lets's Make Change Together
             </h2>
             <h3 className="text-white text-3xl font-normal leading-[72px] tracking-[0.9px] max-sm:text-xl max-[400px]:text-lg">
-                The High-Quality Collection
+                Transforming the lives of millions
             </h3>
             <div className="flex justify-center items-center gap-3 pb-10 max-[400px]:flex-col max-[400px]:gap-1 w-[420px] max-sm:w-[350px] max-[400px]:w-[300px]">
-
+                <button
+                    className="bg-white text-black text-center text-xl border border-[rgba(0, 0, 0, 0.40)] font-normal tracking-[0.6px] leading-[72px] w-full h-12 flex items-center justify-center hover:bg-transparent hover:text-white"
+                >
+                    Join the Cause
+                </button>
+                <h1
+                    className="text-white border-white border-2 text-center text-xl font-normal tracking-[0.6px] leading-[72px] w-full h-12 flex items-center justify-center hover:bg-gray-100 hover:text-black"
+                >
+                    Get start
+                </h1>
             </div>
         </div>
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,23 +2,20 @@ import React from "react";
 
 export const Navbar = () => {
     return (
-        <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
-            <nav className="flex w-full screen-max-width">
-                SOCIAL CAUSE
-                <div className="flex flex-1 justify-center max-sm:hidden">
-                    <div className="px-5 text-sm cursor-pointer text-gray hover:text-white transition-all">
+        <header className="w-full py-5 sm:px-10 px-5 flex border border-b-gray-200 mb-4 justify-between items-center">
+            <nav className="flex w-full text-3xl font-bold screen-max-width">
+                Terntribe build
+                {/* <div className="flex flex-1 justify-center max-sm:hidden">
+                    <div className="px-3 text-sm cursor-pointer text-gray hover:text-gray-500 transition-all">
                         Home
                     </div>
-                    <div className="px-5 text-sm cursor-pointer text-gray hover:text-white transition-all">
+                    <div className="px-3 text-sm cursor-pointer text-gray hover:text-gray-500 transition-all">
                         About
                     </div>
-                    <div className="px-5 text-sm cursor-pointer text-gray hover:text-white transition-all">
+                    <div className="px-3 text-sm cursor-pointer text-gray hover:text-gray-500 transition-all">
                         Contact
                     </div>
-                </div>
-                <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
-
-                </div>
+                </div> */}
             </nav>
         </header>
     );
